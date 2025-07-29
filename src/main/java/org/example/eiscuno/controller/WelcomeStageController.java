9b8cfbdaed84f91613612b64bf036a5c96a2e52a
juanPabloPiedrahita <ccatus10@gmail.com>
2025-07-29 15:33:08 -0500

    New welcome window added

---
+++ src/main/java/org/example/eiscuno/controller/WelcomeStageController.java
@@ -0,0 +1,72 @@
+package org.example.eiscuno.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.*;
+import org.example.eiscuno.model.planeTextFiles.PlaneTextFileHandler;
+import org.example.eiscuno.view.GameUnoStage;
+import org.example.eiscuno.view.WelcomeStage;
+
+import java.io.IOException;
+import java.util.Stack;
+
+public class WelcomeStageController {
+
+    @FXML
+    private StackPane rootPane;
+    @FXML
+    private Button playButton;
+    @FXML
+    private Button continueButton;
+    @FXML
+    private Button quitButton;
+    @FXML
+    private Button creditsButton;
+    @FXML
+    private TextField usernameField;
+
+    private String nickName;
+    private PlaneTextFileHandler planeTextFileHandler;
+
+    @FXML
+    public void initialize() {
+        nickName = "";
+        planeTextFileHandler = new PlaneTextFileHandler();
+    }
+
+    @FXML
+    public void onHandlePlayButton() throws IOException {
+        if(!usernameField.getText().isEmpty()) {
+            nickName = usernameField.getText();
+            WelcomeStage.deleteInstance();
+            GameUnoStage.getInstance();
+        }
+        else {
+            Alert alert = new Alert(Alert.AlertType.WARNING, "¡Enter a username before continuing!");
+            DialogPane dialogPane = alert.getDialogPane();
+            dialogPane.getStylesheets().add(getClass().getResource("/org/example/eiscuno/styles.css").toExternalForm());
+            dialogPane.getStyleClass().add("warning-label");
+            alert.setTitle("Warning");
+            alert.showAndWait();
+        }
+    }
+
+    @FXML
+    public void onHandleContinueButton(){
+
+    }
+
+    @FXML
+    public void onHandleQuitButton(){
+        WelcomeStage.deleteInstance();
+        System.exit(0);
+    }
+
+    @FXML
+    public void onHandleCreditsButton(){
+
+    }
+}
---
+++ src/main/java/org/example/eiscuno/model/planeTextFiles/PlaneTextFileHandler.java
@@ -0,0 +1,33 @@
+package org.example.eiscuno.model.planeTextFiles;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileReader;
+
+public class PlaneTextFileHandler implements IPlaneTextFileHandler {
+    @Override
+    public void write(String filename, String content) throws IOException{
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(filename))){
+            writer.write(content);
+            writer.flush();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String[] read(String filename) {
+        StringBuilder content = new StringBuilder();
+        try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
+            String line;
+            while ((line = reader.readLine()) != null){
+                content.append(line).append(",");
+            }
+        }catch (IOException e ){
+            e.printStackTrace();
+        }
+        return content.toString().split(",");
+    }
+}
